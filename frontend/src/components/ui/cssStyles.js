d482f32d0d267e2ce2b6a1398d282fc9dbeb9841
andrewsapognikov <aindri821@gmail.com>
2023-07-31 01:45:57 +0300

    version 0.1.1

---
+++ frontend/src/components/ui/cssStyles.js
@@ -0,0 +1,15 @@
+export const primaryTextField = {
+    '& .MuiInputBase-root':{
+        borderRadius: '12px',
+    },
+    '& .MuiInputBase-input': {
+        fontSize: '14px',
+        fontFamily: 'Montserrat',
+        paddingLeft: '15px',
+    },
+}
+
+export const primaryButton = {
+    padding: '10px 35px',
+    borderRadius: '12px',
+}
